07e04dcb77a6bdcba0065fb54a3ff70da5e90560
Micah Pierson <Micah.Pierson@outlook.com>
2022-08-09 17:32:52 -0600

    Fixed logic that was causing array related runtime exceptions. Added spacing so each word or command line argument has a space between itself and the others.

--- ConsoleStuff/ConsoleNoteMain.cs
+++ ConsoleStuff/ConsoleNoteMain.cs
@@ -1,62 +1,102 @@
-﻿using static System.Console;
+﻿namespace ConsoleStuff;
 
-namespace ConsoleStuff
+public class ConsoleClass1
 {
-    public class ConsoleClass1
+    static string? fileName;
+    static string? fileContent;
+    static string? command;
+    static string currentDirectory = Environment.CurrentDirectory;
+    static bool argsBig = false;
+    static List<string> content;
+    public static void Main(string[] args)
     {
-        static string? fileName;
-        static string? fileContent;
-        static string? command;
-        static string currentDirectory = Environment.CurrentDirectory;
-        public static void Main(string[] args)
+        try
         {
             command = args[0];
             fileName = args[1];
+        } catch (Exception e2)
+        {
+            Console.WriteLine(e2.Message);
+            Console.WriteLine(e2.Source);
+            Console.WriteLine(e2.InnerException);
+        }
 
+        if (args.Length > 3)
+        {
+            content = new List<string>(args.Count());
+            argsBig = true;
+            content = ArgumentExtension.BigCopy(args);
+                //.GetRange(3, args.Length - 2);
+        }
+        else
+        {
+            argsBig = false;
             try
             {
                 fileContent = args[2];
             }
             catch (Exception e)
-            { WriteLine(e.Message); }
-
-            if (fileName == null)
             {
-                WriteLine("Enter the file name you want to create or change");
-                fileName = ReadLine();
+                Console.WriteLine(e.Message);
             }
-
-            if (command.Contains("Create", StringComparison.CurrentCultureIgnoreCase))
-            {
-                CreateFile(fileName);
-            }
-            else if (command.Contains("Write", StringComparison.CurrentCultureIgnoreCase))
-            {
-                WriteToFile(fileName, fileContent);
-            }
-            else
-            {
-                WriteLine("Please enter either 'Create\' or 'Write'.");
-            }
-
-            ReadLine();
         }
 
-        static void CreateFile(string @fileName)
+
+        if (command == null)
         {
-            File.Create(currentDirectory + @$"\{fileName}" + ".txt");
+            Console.WriteLine("Please enter command: create or write");
+            command = Console.ReadLine();
         }
 
-        static void WriteToFile(string @fileName, string @fileContent)
+        if (fileName == null)
         {
-            if (fileName.Contains(".txt", StringComparison.CurrentCultureIgnoreCase))
+            Console.WriteLine("Enter the file name you want to create or change");
+            fileName = Console.ReadLine();
+        }
+
+        if (command.Contains("Create", StringComparison.CurrentCultureIgnoreCase))
+        {
+            CreateFile(fileName);
+        }
+        else if (command.Contains("Write", StringComparison.CurrentCultureIgnoreCase))
+        {
+            WriteToFile(fileName, fileContent, content);
+        }
+        else
+        {
+            Console.WriteLine("Please enter either 'Create' or 'Write'.");
+        }
+
+        Console.ReadLine();
+    }
+
+    static void CreateFile(string @fileName)
+    {
+        File.Create(currentDirectory + @$"\{fileName}" + ".txt");
+    }
+
+    static void WriteToFile(string @fileName, string @fileContent, List<string> @content)
+    {
+        if (argsBig && @fileName.Contains(".txt", StringComparison.CurrentCultureIgnoreCase))
+        {
+            foreach (string i in @content)
             {
-                File.AppendAllText(fileName, fileContent);
+                File.AppendAllText(fileName, i);
             }
-            else if (!fileName.Contains(".txt", StringComparison.CurrentCultureIgnoreCase))
+        } else if (argsBig && !fileName.Contains(".txt", StringComparison.CurrentCultureIgnoreCase))
+        {
+            foreach (string j in content)
             {
-                File.AppendAllText(@fileName + ".txt", @fileContent);
+                File.AppendAllText(fileName + ".txt", j);
             }
+        }
+        else if (!argsBig && fileName.Contains(".txt", StringComparison.CurrentCultureIgnoreCase))
+        {
+            File.AppendAllText(fileName, fileContent);
+        }
+        else if (!argsBig && !fileName.Contains(".txt", StringComparison.CurrentCultureIgnoreCase))
+        {
+            File.AppendAllText(@fileName + ".txt", @fileContent);
         }
     }
 }
